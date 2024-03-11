3818c81b90b27b7a892dfb1517b452603ea11a24
Akshay Jain <jainakshay1783@gmail.com>
2024-03-10 20:04:29 -0700

    Login Page Header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,14 @@
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="w-full p-2 relative">
+      <img
+        className="w-24 sm:w-52 contrast-150"
+        alt="logo"
+        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+      />
+      <div className="absolute top-0 right-0 bottom-0 left-0 bg-gradient-to-b from-black opacity-50 -z-10"></div>
+    </div>
+  );
 };
 
 export default Header;
--- src/pages/Login.page.js
+++ src/pages/Login.page.js
@@ -1,5 +1,16 @@
+import Header from "../components/Header";
+
 const LoginPage = () => {
-  return <div>Login Page</div>;
+  return (
+    <div className="">
+      <div
+        className="absolute top-0 left-0 right-0 w-full h-full bg-black -z-10
+        md:brightness-50
+        md:bg-[url(https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/fa6f97d9-245e-43d7-bb56-af27cbf6d656/US-en-20240226-popsignuptwoweeks-perspective_alpha_website_large.jpg)]"
+      ></div>
+      <Header />
+    </div>
+  );
 };
 
 export default LoginPage;
